d02cca97faaadbf2bf1b85ca57ca6e806e2b8d3c
Louis Goessling <louis@goessling.com>
2017-11-25 21:28:56 -0600

    fullscreen

--- game/main.py
+++ game/main.py
@@ -1,9 +1,10 @@
 import pygame
 import game
 import level_test
+import sys
 
 SIZE = (1700,900)
 
 pygame.init()
-game = game.JouleThiefGame(pygame.display.set_mode(SIZE, pygame.HWSURFACE|pygame.DOUBLEBUF|pygame.FULLSCREEN))
+game = game.JouleThiefGame(pygame.display.set_mode(SIZE, pygame.FULLSCREEN if "fullscreen" in sys.argv else 0))
 game.set_state(level_test.LevelExample()).mainloop()
