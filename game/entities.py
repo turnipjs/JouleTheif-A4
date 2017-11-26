c289907567043b0448b6faf9dfbc940d80a43fd0
Louis Goessling <louis@goessling.com>
2017-11-26 00:04:49 -0600

    what even is math

--- game/entities.py
+++ game/entities.py
@@ -23,6 +23,6 @@ class ParralaxScroller(game.Entity):
 
 	def draw(self, dt):
 		num_required = 3+(self.level.surf.get_size()[0]//self.image.get_width())
-		p_offset=self.level.scroll_pos[0]*self.factor
+		p_offset=self.level.scroll_pos[0]*-self.factor
 		for idx in range(num_required+2):
-			self.level.surf.blit(self.image, (p_offset+(self.x+self.image.get_width()*idx), self.y))
+			self.level.surf.blit(self.image, (p_offset+(self.x+self.image.get_width()*(idx-1)), self.y))
