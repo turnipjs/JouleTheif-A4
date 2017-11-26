3b876130db1da54ec7032b03f441992e4f6111cb
turnipjs <shmincky1@gmail.com>
2017-11-25 21:28:08 -0600

    fullscreen fixes mac issues

--- game/main.py
+++ game/main.py
@@ -5,5 +5,5 @@ import level_test
 SIZE = (1700,900)
 
 pygame.init()
-game = game.JouleThiefGame(pygame.display.set_mode(SIZE))
+game = game.JouleThiefGame(pygame.display.set_mode(SIZE, pygame.HWSURFACE|pygame.DOUBLEBUF|pygame.FULLSCREEN))
 game.set_state(level_test.LevelExample()).mainloop()
