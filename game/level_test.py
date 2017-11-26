fae0e91c531e3091f18737110a5b2adc6201d816
Louis Goessling <louis@goessling.com>
2017-11-25 22:01:10 -0600

    level change:

--- game/level_test.py
+++ game/level_test.py
@@ -6,5 +6,11 @@ import levels
 
 class LevelExample(levels.Level):
 	def on_setup(self):
-		self.load_level("../sample-level.png")
+		self.load_level("test_level.png")
 		self.create_player((300,300))
+		self.add_entity(creatures.SimpleImageEntity(path="redsquare.png", x=1320, y=740)).on_collided_with=lambda o:self.game.set_state(LevelExample2())
+
+class LevelExample2(levels.Level):
+	def on_setup(self):
+		self.load_level("../sample-level.png")
+		self.create_player((200,200))
