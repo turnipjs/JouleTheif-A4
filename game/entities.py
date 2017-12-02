f15448992865e037f7389f4d6b36856a1da9da9a
Kevin Moonen <dragonrider7225@gmail.com>
2017-12-01 21:25:43 -0600

    Fixed error on creation of SIE or PS with default args.

--- game/entities.py
+++ game/entities.py
@@ -4,8 +4,11 @@ import pygame
 class SimpleImageEntity(game.Entity):
 	def __init__(self, path=None, **k):
 		game.Entity.__init__(self, **k)
-		self.image = pygame.image.load(path).convert()
-		self.image.set_colorkey((255,0,255))
+		if path:
+			self.image = pygame.image.load(path).convert()
+		else:
+			self.image = Surface((0, 0))
+		self.image.set_colorkey((255, 0, 255))
 		self.size = self.image.get_size()
 
 	def draw(self, dt):
@@ -14,15 +17,18 @@ class SimpleImageEntity(game.Entity):
 class ParallaxScroller(game.Entity):
 	def __init__(self, path=None, image=None, factor=0.75, **k):
 		game.Entity.__init__(self, **k)
-		self.image=image
 		if path:
 			self.image = pygame.transform.scale2x(pygame.image.load(path).convert_alpha())
-			self.image.set_colorkey((255,0,255))
+		else:
+			self.image = image
+		if self.image is None:
+			self.image = Surface((0, 0))
+		self.image.set_colorkey((255, 0, 255))
 		self.size = self.image.get_size()
-		self.factor=factor
+		self.factor = factor
 
 	def draw(self, dt):
 		num_required = 3+(self.level.surf.get_size()[0]//self.image.get_width())
-		p_offset=self.level.scroll_pos[0]*-self.factor
-		for idx in range(num_required+2):
-			self.level.surf.blit(self.image, (p_offset+(self.x+self.image.get_width()*(idx-1)), self.y))
+		p_offset = self.level.scroll_pos[0] * -self.factor
+		for idx in range(num_required + 2):
+			self.level.surf.blit(self.image, (p_offset + (self.x + self.image.get_width() * (idx - 1)), self.y))
